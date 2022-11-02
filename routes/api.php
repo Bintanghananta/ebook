48c92671ad2bdaa4cdc920a5aed2edd438551eaa
Bintanghananta <hanantabintang@gmail.com>
2022-11-02 10:28:09 +0700

    auth

--- routes/api.php
+++ routes/api.php
@@ -1,23 +1,23 @@
 <?php
 
-use App\Http\Controllers\SiswaController;
-use App\Http\Controllers\BookController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SiswaController;
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\AuthController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+// Public routes
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::get('/books', [BookController::class, 'index']);
+Route::get('/books/{id}', [BookController::class, 'show']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::resource('books', BookController::class)->except('create', 'edit', 'show', 'index');
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::apiResource('siswa', SiswaController::class);
-Route::apiResource('books', BookController::class);
